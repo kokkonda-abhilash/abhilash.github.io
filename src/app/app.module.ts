39ee3b6f9ad41653e37d08f80e92e545eee7978a
Charry <AG44284@ad.wellpoint.com>
2019-10-18 19:03:04 +0530

    Intial draft of homepage and about me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutmeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
