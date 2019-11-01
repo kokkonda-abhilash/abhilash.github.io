3b91e90aa352cbc80bd7f6b0077e0b0f44b4924e
Charry <AG44284@ad.wellpoint.com>
2019-11-01 22:10:04 +0530

    About me page changes

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { DownloadresumeService } from '../downloadresume.service';
-// tslint:disable-next-line: import-blacklist
-import 'rxjs/Rx';
 
 @Component({
   selector: 'app-aboutme',
@@ -14,15 +12,11 @@ export class AboutmeComponent implements OnInit {
 
   downloadResume(): void {
     this.resumeService.download()
-      .subscribe(response => this.downloadFile(response),
-      error => console.log('error downloading the resume' + error),
+      .subscribe(response => {
+        console.log(response);
+      },
+      error => console.log('error downloading the resume' + error.toString()),
       () => console.log('downloaded resume'));
-  }
-
-  private downloadFile(data): void {
-    const blob = new Blob([data], {type: 'application/octet-stream'});
-    const url = window.URL.createObjectURL(blob);
-    window.open(url);
   }
 
   ngOnInit() {
--- src/app/downloadresume.service.ts
+++ src/app/downloadresume.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,11 +7,28 @@ import { Observable } from 'rxjs';
 })
 export class DownloadresumeService {
 
-  private readonly url = '/assets/documents/resume.docx';
+  private readonly url = 'https://raw.githubusercontent.com/mstraining/resume/master/DBS-Profile-Updated.pdf';
 
   constructor(private http: HttpClient) { }
 
   download(): Observable<any> {
-    return this.http.get(this.url);
+    return this.http.get(this.url, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        Host: 'raw.githubusercontent.com',
+        Connection: 'keep-alive',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36',
+        'Sec-Fetch-User': '?1',
+        Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Sec-Fetch-Site': 'cross-site',
+        'Sec-Fetch-Mode': 'navigate',
+        Referer: 'https://github.com/mstraining/resume/blob/master/DBS-Profile-Updated.pdf',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'If-None-Match': 'W/82b2377659bdee7db5680110a8ea6463ae12067ce7fc34885d0a19a350be9385',
+        'Access-Control-Allow-Methods': 'DELETE, POST, GET, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Accept, Referer'
+      }
+    });
   }
 }
